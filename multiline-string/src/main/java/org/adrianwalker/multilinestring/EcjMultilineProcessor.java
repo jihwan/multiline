b2df12410cd3bd683e4326b9566b575c1d6408d3
jihwan <zhwan.hwang@gmail.com>
2017-03-08 20:08:20 +0900

    UPDATE : EcjMultilineProcessor.java, JavacMultilineProcessor.java : refactor

--- multiline-string/src/main/java/org/adrianwalker/multilinestring/EcjMultilineProcessor.java
+++ multiline-string/src/main/java/org/adrianwalker/multilinestring/EcjMultilineProcessor.java
@@ -2,33 +2,31 @@ package org.adrianwalker.multilinestring;
 
 import java.util.Set;
 
-import javax.annotation.processing.AbstractProcessor;
 import javax.annotation.processing.ProcessingEnvironment;
 import javax.annotation.processing.RoundEnvironment;
-import javax.annotation.processing.SupportedAnnotationTypes;
 import javax.lang.model.element.Element;
 import javax.lang.model.element.TypeElement;
 import javax.lang.model.util.Elements;
 
+import org.adrianwalker.multilinestring.support.StringProcessorStrategy;
 import org.eclipse.jdt.internal.compiler.apt.model.VariableElementImpl;
 import org.eclipse.jdt.internal.compiler.ast.FieldDeclaration;
 import org.eclipse.jdt.internal.compiler.ast.StringLiteral;
 import org.eclipse.jdt.internal.compiler.lookup.FieldBinding;
 
-@SupportedAnnotationTypes({"org.adrianwalker.multilinestring.Multiline"})
-public final class EcjMultilineProcessor extends AbstractProcessor {
+public final class EcjMultilineProcessor extends AbstractMultilineProcessor {
 
 	private Elements elementUtils;
-  
+	
 	@Override
 	public void init(final ProcessingEnvironment procEnv) {
 		super.init(procEnv);
 		this.elementUtils = procEnv.getElementUtils();
 	}
-
+	
 	@Override
-	public boolean process(final Set<? extends TypeElement> annotations, final RoundEnvironment roundEnv) {
-		Set<? extends Element> fields = roundEnv.getElementsAnnotatedWith(Multiline.class);
+	void processMultiline(final RoundEnvironment roundEnv, TypeElement annotation, StringProcessorStrategy strategy) {
+		Set<? extends Element> fields = roundEnv.getElementsAnnotatedWith(annotation);
 
 		for (Element field : fields) {
 			String docComment = elementUtils.getDocComment(field);
@@ -37,10 +35,13 @@ public final class EcjMultilineProcessor extends AbstractProcessor {
 				VariableElementImpl fieldElem = (VariableElementImpl) field;
 				FieldBinding biding = (FieldBinding) fieldElem._binding;
 				FieldDeclaration decl = biding.sourceField();
-				StringLiteral string = new StringLiteral(StringProcessor.toString(docComment,fieldElem.getAnnotation(Multiline.class)).toCharArray(), decl.sourceStart ,decl.sourceEnd, decl.sourceStart);
+				StringLiteral string = new StringLiteral(
+						strategy.toString(docComment, fieldElem).toCharArray(), 
+						decl.sourceStart,
+						decl.sourceEnd, 
+						decl.sourceStart);
 				decl.initialization = string;
 			}
 		}
-		return true;
 	}
 }
--- multiline-string/src/main/java/org/adrianwalker/multilinestring/JavacMultilineProcessor.java
+++ multiline-string/src/main/java/org/adrianwalker/multilinestring/JavacMultilineProcessor.java
@@ -2,25 +2,23 @@ package org.adrianwalker.multilinestring;
 
 import java.util.Set;
 
-import javax.annotation.processing.AbstractProcessor;
 import javax.annotation.processing.ProcessingEnvironment;
 import javax.annotation.processing.RoundEnvironment;
-import javax.annotation.processing.SupportedAnnotationTypes;
-import javax.lang.model.SourceVersion;
 import javax.lang.model.element.Element;
 import javax.lang.model.element.TypeElement;
+
+import org.adrianwalker.multilinestring.support.StringProcessorStrategy;
 
 import com.sun.tools.javac.model.JavacElements;
 import com.sun.tools.javac.processing.JavacProcessingEnvironment;
 import com.sun.tools.javac.tree.JCTree.JCVariableDecl;
 import com.sun.tools.javac.tree.TreeMaker;
 
-@SupportedAnnotationTypes({"org.adrianwalker.multilinestring.Multiline"})
-public final class JavacMultilineProcessor extends AbstractProcessor {
+public final class JavacMultilineProcessor extends AbstractMultilineProcessor {
 
 	private JavacElements elementUtils;
 	private TreeMaker maker;
-  
+	
 	@Override
 	public void init(final ProcessingEnvironment procEnv) {
 		super.init(procEnv);
@@ -28,21 +26,20 @@ public final class JavacMultilineProcessor extends AbstractProcessor {
 		this.elementUtils = javacProcessingEnv.getElementUtils();
 		this.maker = TreeMaker.instance(javacProcessingEnv.getContext());
 	}
-
-	@Override public SourceVersion getSupportedSourceVersion() {
-               return SourceVersion.latest();
-        }
-
+	
 	@Override
-	public boolean process(final Set<? extends TypeElement> annotations, final RoundEnvironment roundEnv) {
-		Set<? extends Element> fields = roundEnv.getElementsAnnotatedWith(Multiline.class);
+	void processMultiline(final RoundEnvironment roundEnv, TypeElement annotation, StringProcessorStrategy strategy) {
+		Set<? extends Element> fields = roundEnv.getElementsAnnotatedWith(annotation);
+
 		for (Element field : fields) {
 			String docComment = elementUtils.getDocComment(field);
+		
 			if (null != docComment) {
+				
 				JCVariableDecl fieldNode = (JCVariableDecl) elementUtils.getTree(field);
-				fieldNode.init = maker.Literal(StringProcessor.toString(docComment,field.getAnnotation(Multiline.class)));
+				fieldNode.init = maker
+						.Literal(strategy.toString(docComment, field));
 			}
 		}
-		return true;
 	}
 }
