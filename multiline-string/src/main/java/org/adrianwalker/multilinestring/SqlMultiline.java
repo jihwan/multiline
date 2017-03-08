d22f334a44af9a74ee57731bc651313081b494be
jihwan <zhwan.hwang@gmail.com>
2017-03-08 20:00:45 +0900

    ADD : SqlMultiline.java : initial add

---
+++ multiline-string/src/main/java/org/adrianwalker/multilinestring/SqlMultiline.java
@@ -0,0 +1,16 @@
+package org.adrianwalker.multilinestring;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author Jihwan Hwang
+ */
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.SOURCE)
+public @interface SqlMultiline {
+	boolean mergeLines() default false;
+	char mergeChar() default ' ';
+}
